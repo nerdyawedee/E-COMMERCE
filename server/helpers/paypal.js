f09891cf889c1cb0a27800fa89f17ffb008f2327
Debasmita <121428321+nerdyawedee@users.noreply.github.com>
2025-03-09 07:13:27 +0530

    Update paypal.js

--- server/helpers/paypal.js
+++ server/helpers/paypal.js
@@ -1,9 +1,9 @@
 const paypal = require("paypal-rest-sdk");
 
 paypal.configure({
-  mode: "sandbox",
-  client_id: "AU41gfVlkLnoNbSMwXzHhXhQQvCH1Q3-7cG9DWaU78CuFkHyYP9nx2T6JN4Cx-PeFYqUqRvmZ5Szsbs1",
-  client_secret: "EPT6onRVipCqWdl49n4a3oUl6FBldMyWm1uFFXOhsHmsSN7lbJqtncU9fITF90zXrLDr85Bo-3tR-0t5",
+  mode: "",
+  client_id: "",
+  client_secret: "",
 });
 
 module.exports = paypal;
